3a3af0e6eb6407fac9f4a9b9c33b06c1d63de3e1
Farid Azhari <faridazhari07@gmail.com>
2025-06-02 03:12:08 +0700

    add comment user api

--- src/route/api.js
+++ src/route/api.js
@@ -4,6 +4,8 @@ import {authMiddleware} from "../middleware/auth-middleware.js";
 
 const userRouter = new express.Router();
 userRouter.use(authMiddleware)
+
+// User API
 userRouter.get('/api/users/current', userController.get);
 userRouter.patch('/api/users/current', userController.update);
 userRouter.delete('/api/users/logout', userController.logout);
