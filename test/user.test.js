41dc76a40e47fe895b0a1d14cb1a031b3d848eb0
Farid Azhari <faridazhari07@gmail.com>
2025-06-01 19:20:16 +0700

    fix update user test

--- test/user.test.js
+++ test/user.test.js
@@ -182,13 +182,13 @@ describe('PATCH /api/users/current', function () {
             .patch("/api/users/current")
             .set("Authorization", "test")
             .send({
-                name: "Eko",
+                name: "Farid",
                 password: "rahasialagi"
             });
 
         expect(result.status).toBe(200);
         expect(result.body.data.username).toBe("test");
-        expect(result.body.data.name).toBe("Eko");
+        expect(result.body.data.name).toBe("Farid");
 
         const user = await getTestUser();
         expect(await bcrypt.compare("rahasialagi", user.password)).toBe(true);
@@ -199,12 +199,12 @@ describe('PATCH /api/users/current', function () {
             .patch("/api/users/current")
             .set("Authorization", "test")
             .send({
-                name: "Eko"
+                name: "Farid"
             });
 
         expect(result.status).toBe(200);
         expect(result.body.data.username).toBe("test");
-        expect(result.body.data.name).toBe("Eko");
+        expect(result.body.data.name).toBe("Farid");
     });
 
     it('should can update user password', async () => {
