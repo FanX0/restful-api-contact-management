285816f6d48ac9499d01d88e140ac9f42bab97d6
Farid Azhari <faridazhari07@gmail.com>
2025-06-06 19:34:20 +0700

    add update contact test

--- test/contact.test.js
+++ test/contact.test.js
@@ -83,3 +83,68 @@ describe('GET /api/contacts/:contactId', function () {
         expect(result.status).toBe(404);
     });
 });
+
+describe('PUT /api/contacts/:contactId', function () {
+    beforeEach(async () => {
+        await createTestUser();
+        await createTestContact();
+    })
+
+    afterEach(async () => {
+        await removeAllTestContacts();
+        await removeTestUser();
+    })
+
+    it('should can update existing contact', async () => {
+        const testContact = await getTestContact();
+
+        const result = await supertest(web)
+            .put('/api/contacts/' + testContact.id)
+            .set('Authorization', 'Bearer test')
+            .send({
+                first_name: "Farid",
+                last_name: "Azhari",
+                email: "farid@test.com",
+                phone: "09999999"
+            });
+
+        expect(result.status).toBe(200);
+        expect(result.body.data.id).toBe(testContact.id);
+        expect(result.body.data.first_name).toBe("Farid");
+        expect(result.body.data.last_name).toBe("Azhari");
+        expect(result.body.data.email).toBe("farid@test.com");
+        expect(result.body.data.phone).toBe("09999999");
+    });
+
+    it('should reject if request is invalid', async () => {
+        const testContact = await getTestContact();
+
+        const result = await supertest(web)
+            .put('/api/contacts/' + testContact.id)
+            .set('Authorization', 'Bearer test')
+            .send({
+                first_name: "",
+                last_name: "",
+                email: "farid",
+                phone: ""
+            });
+
+        expect(result.status).toBe(400);
+    });
+
+    it('should reject if contact is not found', async () => {
+        const testContact = await getTestContact();
+
+        const result = await supertest(web)
+            .put('/api/contacts/' + (testContact.id + 1))
+            .set('Authorization', 'Bearer test')
+            .send({
+                first_name: "Farid",
+                last_name: "Azhari",
+                email: "farid@test.com",
+                phone: "09999999"
+            });
+
+        expect(result.status).toBe(404);
+    });
+});
